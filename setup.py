f683fea21856fdd50c1d2ae937a120d3607adffb
Lloyd Konneker <bootch@nc.rr.com>
2014-04-08 14:50:47 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='qtPrintFramework',
+      version='0.1',
+      description='Print subsystem wrapping Qt print system',
+      author='Lloyd Konneker',
+      author_email='bootch@nc.rr.com',
+      url='https://github.com/bootchk/qtPrintFramework',
+      packages=['qtPrintFramework',
+                'qtPrintFramework.model',
+                'qtPrintFramework.userInterface', ]
+     )
